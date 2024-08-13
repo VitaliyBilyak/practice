bce33de72704a5e414810a5a07f9c0ae20ccf222
VitaliyBilyak <124344952+VitaliyBilyak@users.noreply.github.com>
2024-08-13 19:50:25 +0300

    Delete django directory

--- django/monthly_challenges/challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from  . import  views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:month>", views.mounthly_challenge_by_number),
-    path("<str:month>", views.mounthly_challenge, name="month-challenge")
-]
